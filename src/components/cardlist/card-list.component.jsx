0c9bf10f152a20de0d6a3ca980a8dad4dfa52089
Zachary Lynn <zalynn25@hotmail.com>
2021-01-17 21:50:07 -0500

    update

---
+++ src/components/cardlist/card-list.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Card } from '../card/card.component';
+
+import '.card/card-list.styles.css';
+
+export const CardList = props => (
+    <div className= 'card-list'>
+
+    </div>
+)
