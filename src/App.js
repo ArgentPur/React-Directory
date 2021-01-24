da2af1a9213046a2fc8016d6cfa247166136cbce
Zachary Lynn <zalynn25@hotmail.com>
2021-01-24 10:47:45 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import CardList from './components/card/card-list.component.jsx'
+import CardList from './components/cardlist/card-list.component.jsx'
 import Wrapper from './components/wrapper/wrapper.jsx'
 import './App.css';
 
